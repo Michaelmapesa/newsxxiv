60fe115074ecf481b516075c64285d685dd70b46
michael mapesa <michaelmapesa93@gmail.com>
2022-02-26 21:20:28 +0300

    creating route

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from newsapi import NewsApiClient
+app = Flask(__name__)
+@app.route('/')
+def index():
+    #client apikey and id for authorization
+    newsapi = NewsApiClient(api_key="e42fd1790bc54e73bb51cfcd7f903e9a")
+    top_headlines = newsapi.get_top_headlines(sources="ktn-news")
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
